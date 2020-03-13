1ca991670d67bb87aba3f449f7c4d21b34310305
ADFDBSERVER <ADFDBSERVER@DESKTOP-M6O26BQ>
2020-03-13 17:34:13 +0500

    general setup issue solved

--- ViewController/src/view/SideBar/Setup.java
+++ ViewController/src/view/SideBar/Setup.java
@@ -7,18 +7,11 @@ import javax.faces.event.ActionEvent;
 import oracle.adf.controller.TaskFlowId;
 
 public class Setup implements Serializable {
-    private String taskFlowId = "/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Setup/Setup_TaskFlow.xml#Setup_TaskFlow";
+    private String taskFlowId1 =
+        "/WEB-INF/TaskFlows/Group/Company/SBU/Modules/General_Setup/General_Setup_TaskFlow.xml#Setup_TaskFlow";
 
     public Setup() {
-    }
-
-    public TaskFlowId getDynamicTaskFlowId() {
-        return TaskFlowId.parse(taskFlowId);
-    }
-
-    public void setDynamicTaskFlowId(String taskFlowId) {
-        this.taskFlowId = taskFlowId;
-    }    
+    }   
     
     public void fragmentCall(ActionEvent actionEvent) {
         // Add event code here...
@@ -28,6 +21,14 @@ public class Setup implements Serializable {
         link.append(action);
         link.append(".xml#");
         link.append(action);
-        setDynamicTaskFlowId(link.toString());
+        setDynamicTaskFlowId1(link.toString());
+    }
+
+    public TaskFlowId getDynamicTaskFlowId1() {
+        return TaskFlowId.parse(taskFlowId1);
+    }
+
+    public void setDynamicTaskFlowId1(String taskFlowId) {
+        this.taskFlowId1 = taskFlowId;
     }
 }
