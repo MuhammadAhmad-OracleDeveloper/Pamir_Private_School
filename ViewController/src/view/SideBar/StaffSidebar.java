fc8a722a6774d1165400adefdc97068f7cfc0151
OracleServer <OracleServer@DESKTOP-F1UE112>
2020-03-16 06:00:14 -0700

    qasim resolving conflicts

---
+++ ViewController/src/view/SideBar/StaffSidebar.java
@@ -0,0 +1,32 @@
+package view.SideBar;
+
+import java.io.Serializable;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.controller.TaskFlowId;
+
+public class StaffSidebar implements Serializable {
+    private String taskFlowId = "/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Staff/Staff.xml#Staff";
+
+    public StaffSidebar() {
+    }
+
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
+    }
+
+    public void setDynamicTaskFlowId(String taskFlowId) {
+        this.taskFlowId = taskFlowId;
+    }    
+    public void fragmentCall(ActionEvent actionEvent) {
+        // Add event code here...
+        String action = (String)actionEvent.getComponent().getAttributes().get("link");
+        StringBuilder link = new StringBuilder();
+        link.append("/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Staff/");
+        link.append(action);
+        link.append(".xml#");
+        link.append(action);
+        setDynamicTaskFlowId(link.toString());
+    }
+}
