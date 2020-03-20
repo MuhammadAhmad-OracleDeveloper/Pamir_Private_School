59179cd0695147346c92fc35e73a41cad9e69e34
ADFDBSERVER <ADFDBSERVER@DESKTOP-M6O26BQ>
2020-03-20 18:01:23 +0500

    new module Transportation added with setup page

---
+++ ViewController/src/view/SideBar/TransportationSidebar.java
@@ -0,0 +1,34 @@
+package view.SideBar;
+
+import java.io.Serializable;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.controller.TaskFlowId;
+
+public class TransportationSidebar implements Serializable {
+    private String taskFlowId =
+        "/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Transportation/Transportation_TaskFlow.xml#Transportation_TaskFlow";
+
+    public TransportationSidebar() {
+    }
+
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
+    }
+
+    public void setDynamicTaskFlowId(String taskFlowId) {
+        this.taskFlowId = taskFlowId;
+    }
+    
+    public void fragmentCall(ActionEvent actionEvent) {
+        // Add event code here...
+        String action = (String)actionEvent.getComponent().getAttributes().get("link");
+        StringBuilder link = new StringBuilder();
+        link.append("/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Transportation/");
+        link.append(action);
+        link.append(".xml#");
+        link.append(action);
+        setDynamicTaskFlowId(link.toString());
+    }
+}
