948aee4a76070035684c0dc6b521b23b9154b23d
ADFDBSERVER <ADFDBSERVER@DESKTOP-M6O26BQ>
2020-04-20 16:39:45 +0500

    leave module added with 4 pages with their eo vo

---
+++ ViewController/src/view/SideBar/LeavesSidebar.java
@@ -0,0 +1,34 @@
+package view.SideBar;
+
+import java.io.Serializable;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.controller.TaskFlowId;
+
+public class LeavesSidebar implements Serializable {
+    private String taskFlowId =
+        "/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Leaves/Leaves_TaskFlow.xml#Leaves_TaskFlow";
+
+    public LeavesSidebar() {
+    }
+
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
+    }
+
+    public void setDynamicTaskFlowId(String taskFlowId) {
+        this.taskFlowId = taskFlowId;
+    }
+    
+    public void fragmentCall(ActionEvent actionEvent) {
+        // Add event code here...
+        String action = (String)actionEvent.getComponent().getAttributes().get("link");
+        StringBuilder link = new StringBuilder();
+        link.append("/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Leaves/");
+        link.append(action);
+        link.append(".xml#");
+        link.append(action);
+        setDynamicTaskFlowId(link.toString());
+    }
+}
