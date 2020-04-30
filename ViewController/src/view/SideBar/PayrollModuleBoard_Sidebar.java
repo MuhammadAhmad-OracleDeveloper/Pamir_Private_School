6e7a11d5b3019c768d97acbd983a195ff43c4800
ADFDBSERVER <ADFDBSERVER@DESKTOP-M6O26BQ>
2020-04-30 14:58:09 +0500

    payroll module done

---
+++ ViewController/src/view/SideBar/PayrollModuleBoard_Sidebar.java
@@ -0,0 +1,45 @@
+package view.SideBar;
+
+import java.io.Serializable;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.controller.TaskFlowId;
+
+public class PayrollModuleBoard_Sidebar implements Serializable {
+    private String taskFlowId =
+        "/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Payroll/PayrollModuleBoard_TaskFlow.xml#PayrollModuleBoard_TaskFlow";
+
+    public PayrollModuleBoard_Sidebar() {
+    }
+
+    public TaskFlowId getDynamicTaskFlowId() {
+        return TaskFlowId.parse(taskFlowId);
+    }
+
+    public void setDynamicTaskFlowId(String taskFlowId) {
+        this.taskFlowId = taskFlowId;
+    }
+    
+    public void PayrollSetupFragmentCall(ActionEvent actionEvent) {
+        // Add event code here...
+        String action = (String)actionEvent.getComponent().getAttributes().get("link");
+        StringBuilder link = new StringBuilder();
+        link.append("/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Payroll/Payroll_Setup/");
+        link.append(action);
+        link.append(".xml#");
+        link.append(action);
+        setDynamicTaskFlowId(link.toString());
+    }
+    
+    public void LeavesFragmentCall(ActionEvent actionEvent) {
+        // Add event code here...
+        String action = (String)actionEvent.getComponent().getAttributes().get("link");
+        StringBuilder link = new StringBuilder();
+        link.append("/WEB-INF/TaskFlows/Group/Company/SBU/Modules/Payroll/Leaves/");
+        link.append(action);
+        link.append(".xml#");
+        link.append(action);
+        setDynamicTaskFlowId(link.toString());
+    }
+}
