41a0c0df860fe1e0d2149666f3aa7e171799887f
ReportServer <ReportServer@DESKTOP-SMD7KGI>
2020-09-12 11:44:28 +0500

    Attatch Student Ledger Report

--- ViewController/src/view/Report/StudentLedger.java
+++ ViewController/src/view/Report/StudentLedger.java
@@ -40,7 +40,7 @@ public class StudentLedger {
             switch (selectedReportType) {
             case "StdLedger":
 
-                reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Std_Ledger&");
+                reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/STD_Ledger&");
                 break;
 
             
