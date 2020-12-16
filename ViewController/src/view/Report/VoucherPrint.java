eacc62fe73af4258b0d0daafbb4357e8cc1944bc
AdfDeveloper <AdfDeveloper@192.168.2.2>
2020-12-16 16:02:56 +0500

    voucher attachment in process

--- ViewController/src/view/Report/VoucherPrint.java
+++ ViewController/src/view/Report/VoucherPrint.java
@@ -1,5 +1,7 @@
 package view.Report;
 
+import java.math.BigDecimal;
+
 import javax.faces.event.ActionEvent;
 
 import view.DatabaseConnection.DatabaseConnection;
@@ -14,10 +16,10 @@ public class VoucherPrint {
     public void getStdRegReport(ActionEvent actionEvent) {
         // Add event code here...
         String url = "";
-        Number sendStdRegID = (Number) actionEvent.getComponent().getAttributes().get("sendStdRegID");
-        reportBean.setReportParameter("P_StdRegID", sendStdRegID.toString());
+        BigDecimal sendStdRegID = (BigDecimal) actionEvent.getComponent().getAttributes().get("sendStdRegID");
+        reportBean.setReportParameter("P_Std_reg_id", sendStdRegID.toString());
         
-        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Std_Detail&");
+        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Student_Registration_Voucher&");
         reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
                                         "CACHE"); // which will be one of the [cashe - file - mail - printer]
         reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
@@ -32,10 +34,64 @@ public class VoucherPrint {
     public void getStaffReport(ActionEvent actionEvent) {
         // Add event code here...
         String url = "";
-        Number sendStaffID = (Number) actionEvent.getComponent().getAttributes().get("sendStaffID");
-        reportBean.setReportParameter("P_StaffID", sendStaffID.toString());
+        BigDecimal sendStaffID = (BigDecimal) actionEvent.getComponent().getAttributes().get("sendStaffID");
+        reportBean.setReportParameter("P_Staff_id", sendStaffID.toString());
         
-        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Std_Detail&");
+        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Staff_Detail_Voucher&");
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+                                        "CACHE"); // which will be one of the [cashe - file - mail - printer]
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+                                        "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+        reportBean.setReportParameter("paramform", "no");
+
+        url = reportBean.getReportServerURL();
+        System.out.println("Url => " + url);
+        reportBean.openUrlInNewWindow(url);
+    }
+
+    public void getEnquiryFollowUpReport(ActionEvent actionEvent) {
+        // Add event code here...
+        String url = "";
+        BigDecimal sendEnqFollowUpID = (BigDecimal) actionEvent.getComponent().getAttributes().get("sendEnqFollowUpID");
+        reportBean.setReportParameter("P_Enq_Follow_up_id", sendEnqFollowUpID.toString());
+        
+        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Enquiry_Follow_Up_Voucher&");
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+                                        "CACHE"); // which will be one of the [cashe - file - mail - printer]
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+                                        "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+        reportBean.setReportParameter("paramform", "no");
+
+        url = reportBean.getReportServerURL();
+        System.out.println("Url => " + url);
+        reportBean.openUrlInNewWindow(url);
+    }
+
+    public void getGradeAttReport(ActionEvent actionEvent) {
+        // Add event code here...
+        String url = "";
+        BigDecimal sendGradeAttID = (BigDecimal) actionEvent.getComponent().getAttributes().get("sendGradeAttID");
+        reportBean.setReportParameter("P_Grade_Att_M_ID", sendGradeAttID.toString());
+        
+        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Grade_ATT_Voucher&");
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+                                        "CACHE"); // which will be one of the [cashe - file - mail - printer]
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+                                        "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+        reportBean.setReportParameter("paramform", "no");
+
+        url = reportBean.getReportServerURL();
+        System.out.println("Url => " + url);
+        reportBean.openUrlInNewWindow(url);
+    }
+
+    public void getStdEnqReport(ActionEvent actionEvent) {
+        // Add event code here...
+        String url = "";
+        BigDecimal sendStdEnqID = (BigDecimal) actionEvent.getComponent().getAttributes().get("sendStdEnqID");
+        reportBean.setReportParameter("P_Std_Enq_id", sendStdEnqID.toString());
+        
+        reportBean.setReportURLName("userid=ppss/ppss@orcl&domain=classicdomain&report=C:/PPSS_Reports/Student_Enquiry_With_Follow_Up_Voucher&");
         reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
                                         "CACHE"); // which will be one of the [cashe - file - mail - printer]
         reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
